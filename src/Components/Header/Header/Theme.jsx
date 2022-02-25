55398fa6ed689b8793054457009cefe15b44aee8
tramtran0497 <tramtran0497@gmail.com>
2022-02-25 16:13:24 +0200

    Complete style of toggle Theme in Navbar

--- src/Components/Header/Header/Theme.jsx
+++ src/Components/Header/Header/Theme.jsx
@@ -1,16 +1,20 @@
-import { ThemeUpdateContext } from "../../../useContext/ThemeContext.js";
-import React, { useContext } from "react";
+import { ThemeContext, ThemeUpdateContext } from "../../../useContext/ThemeContext.js";
+import React, { useContext} from "react";
+import "../HeaderStyle/theme.scss"
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 function Theme() {
   const toggleTheme = useContext(ThemeUpdateContext);
-
+  const darkTheme = useContext(ThemeContext)
+ 
   return (
     <div className="theme">
-      <p>Light</p>
-      <div className="container">
-        <div className="circle-btn" onClick={toggleTheme}></div>
+      <LightModeIcon/>
+      <div className="theme__oval" onClick={toggleTheme} >
+        <div className="theme__oval__btn" style={{transform: darkTheme ? "translate(16px)" : "translate(0px)"}}></div>
       </div>
-      <p>Dark</p>
+      <DarkModeIcon style={{color: "black"}}/>
     </div>
   );
 }
